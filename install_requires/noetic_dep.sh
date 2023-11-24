1d29174ee516d30fe94b833435e59644eedb9835
syahrulirwansyah12 <ach.syahrul99@gmail.com>
2023-11-24 15:53:01 +0700

    add script to install dependencies

---
+++ install_requires/noetic_dep.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo "[Install ROS Dependencies]"
+sudo apt-get install -y ros-noetic-rosserial-arduino ros-melodic-rosserial
+sudo apt-get install -y ros-noetic-rplidar-ros
+sudo apt-get install -y ros-noetic-slam-karto ros-noetic-hector-slam ros-noetic-gmapping
+sudo apt-get install -y ros-noetic-move-base ros-noetic-dwa-local-planner ros-noetic-robot-localization
+sudo apt-get install -y ros-noetic-topic-tools ros-noetic-explore-lite
+
