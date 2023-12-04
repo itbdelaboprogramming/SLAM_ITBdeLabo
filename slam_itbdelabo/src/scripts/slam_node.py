e22b85945d7c619fa347624d20bae44e82ea1e80
syahrulirwansyah12 <ach.syahrul99@gmail.com>
2023-12-04 19:10:16 +0700

    add wheel_speed msg

--- slam_itbdelabo/src/scripts/slam_node.py
+++ slam_itbdelabo/src/scripts/slam_node.py
@@ -14,16 +14,16 @@ pause_mapping = False
 rospy.init_node('slam_node')
 
 # Create ROS Publisher
-hardware_command_pub = hardware_command_pub = rospy.Publisher('hardware_command', HardwareCommand, queue_size=1)
+hardware_command_pub = rospy.Publisher('hardware_command', HardwareCommand, queue_size=1)
 
 # Create ROS Subscriber
-x_vel = 0.0
-rot_vel = 0.0
+vx = 0.0
+wz = 0.0
 def cmd_vel_callback(msg):
-    global x_vel
-    global rot_vel
-    x_vel = msg.linear.x
-    rot_vel = msg.angular.z
+    global vx
+    global wz
+    vx = msg.linear.x
+    wz = msg.angular.z
 cmd_vel_sub = rospy.Subscriber('cmd_vel', Twist, cmd_vel_callback)
 
 # Create ROS Service
@@ -54,23 +54,33 @@ rospy.Service('set_mapping', SetMapping, handle_mapping)
 compute_period = rospy.get_param("/compute_period")
 max_speed_linear = rospy.get_param("/max_speed_linear")
 max_speed_angular = rospy.get_param("/max_speed_angular")
+wheel_radius = rospy.get_param("/wheel_radius","5.0")		# in cm
+wheel_distance = rospy.get_param("/wheel_radius","5.0")		# in cm
 
 frequency = (1/compute_period) * 1000
 rate = rospy.Rate(frequency)
 
 while not rospy.is_shutdown():
-    # TODO: add logic for start mapping, pause mapping, and stop mapping 
-    
+    # TODO: add logic for start mapping, pause mapping, and stop mapping
     print(f"start_mapping = {start_mapping}, pause_mapping = {pause_mapping}, stop_mapping = {stop_mapping}")
     hardware_command_msg = HardwareCommand()
+    
+    if start_mapping == 1 :
+    	# inverse kinematics
+    	hardware_command_msg.right_motor_speed = (vx*100.0/wheel_radius - wz*wheel_distance/(2.0*wheel_radius))*9.55
+    	hardware_command_msg.left_motor_speed = (vx*100.0/wheel_radius + wz*wheel_distance/(2.0*wheel_radius))*9.55
+    else:
+    	hardware_command_msg.right_motor_speed = 0.0
+    	hardware_command_msg.left_motor_speed = 0.0
+    
     # convention, rot_vel (+) -> clockwise (navigation/compass-based)
-    if x_vel > 0 :
+    if vx > 0 :
         # forward
         hardware_command_msg.movement_command = 3 
-    elif rot_vel < 0:
+    elif wz < 0:
         # left
         hardware_command_msg.movement_command = 2 
-    elif rot_vel > 0:
+    elif wz > 0:
         # right
         hardware_command_msg.movement_command = 1 
     else:
