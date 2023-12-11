9e0f6de293158e933f85e6ea199d067116c7c2f1
farrel-a <13520110@std.stei.itb.ac.id>
2023-12-11 15:46:18 +0700

    fix: dependencies

--- install_requires/noetic_dep.sh
+++ install_requires/noetic_dep.sh
@@ -7,3 +7,4 @@ sudo apt-get install -y ros-noetic-slam-karto ros-noetic-hector-slam ros-noetic-
 sudo apt-get install -y ros-noetic-move-base ros-noetic-dwa-local-planner ros-noetic-robot-localization
 sudo apt-get install -y ros-noetic-topic-tools ros-noetic-explore-lite ros-noetic-tf2-tools
 sudo apt-get install -y ros-noetic-map-server
+sudo apt-get install -y ros-noetic-rosbridge-server
